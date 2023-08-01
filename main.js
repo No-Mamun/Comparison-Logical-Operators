400131f4c086cbb91dd0bd3737dbb4575650a8af
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-08-01 08:03:10 +0600

    Add files via upload
    Comparison & Logical Operators

---
+++ main.js
@@ -0,0 +1,188 @@
+
+// Comparison & Logical Operators
+
+
+/*
+Node:1
+equals to
+const x = 10;
+if (x == 10){
+
+              console.log(x);
+}
+
+*/
+
+/*
+Node:2
+equals value and equals Type
+const x= 10;
+if (x === 10){
+
+              console.log(x);
+}
+*/
+
+/*
+Node:3
+const x = '10';
+if (x === '10'){
+
+              console.log(x);
+}
+*/
+
+/*
+Node:4
+Not equals
+
+const x = 100;
+if (x != 10){
+
+              console.log(x);
+}
+
+*/
+
+/*
+Node:5
+Not equals value or not equals Type
+
+const x = '10';
+if (x !== 10){
+
+              console.log(x);
+}
+
+*/
+
+/*
+Node:6
+Greter Than
+
+const x = 11;
+if (x > 10){
+
+              console.log(x);
+}
+
+*/
+
+
+/*
+Node:7
+Greter Than or equal to
+
+const x = 10;
+if (x >= 10){
+
+              console.log(x);
+}
+*/
+
+/*
+Node:8
+Greter Than or equal to
+
+const x = 11;
+if (x >= 10){
+
+              console.log(x);
+}
+
+*/
+
+/*
+Node:9
+Greter Than or equal to
+
+const x = 8;
+if (x < 10){
+
+              console.log(x);
+}
+*/
+
+/*
+Node:10
+Greter Than or equal to
+
+const x = 8;
+if (x < 10){
+
+              console.log(x);
+}
+
+*/
+
+/*
+Node:11
+Greter Than or equal to
+
+const x = 1;
+if (x <= 10){
+
+              console.log(x);
+}
+*/
+
+// (**)Logical Operators ;
+// Operators Description;
+// A, &&   and;
+// B, ||    or;
+// C,! not;
+
+// Node:1
+/*
+const x = 12;
+const y = 10;
+
+if (x == 12 || y == 10){
+
+console.log(x);
+}
+*/
+
+/*
+Node:2
+const x = 11;
+const y = 10;
+
+if (x == 12 || y == 10){
+
+console.log(x);
+}
+*/
+
+/*
+Node:3
+const x = 12;
+const y = 10;
+
+if (x == 12 && y == 10){
+
+console.log(x);
+}
+*/
+
+/*
+Node:4
+const x = 12;
+const y = 10;
+
+if (x != y ){
+
+console.log(x);
+}
+*/
+
+/*
+Node:5
+const x = 10;
+const y = 10;
+
+if (x != y ){
+
+console.log(x);
+}
+*/
